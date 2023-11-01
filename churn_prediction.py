991cededddf8274e000fd3ff753a50886038345f
CCaonguyen <caonguyen2262003@gmail.com>
2023-11-01 08:27:55 +0700

    firts

--- churn_prediction.py
+++ churn_prediction.py
@@ -86,16 +86,4 @@ def process_data(churn_predictor):
 
 
 
-if __name__ == "__main__":
-    churn_predictor1 = ChurnPrediction('churn-bigml-80.csv')
-# Tạo một đối tượng ChurnPrediction với tệp dữ liệu 'churn-bigml-80.csv'.
-    churn_predictor2 = ChurnPrediction('churn-bigml-20.csv')
-# Tạo một đối tượng ChurnPrediction với tệp dữ liệu 'churn-bigml-20.csv'.
 
-# Xử lý dữ liệu và hiển thị báo cáo cho tệp 1.
-    print("Báo cáo cho tệp churn-bigml-80.csv")
-    process_data(churn_predictor1)
-
-# Xử lý dữ liệu và hiển thị báo cáo cho tệp 2.
-    print("Báo cáo cho tệp churn-bigml-20.csv")
-    process_data(churn_predictor2)
--- main.py
+++ main.py
@@ -1,57 +1,20 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from churn_prediction import ChurnPrediction, process_data
 
-# Bước 1: Đọc và Khám phá dữ liệu
-data = pd.read_csv('churn-bigml-80.csv')
+def main():
 
-# Xem vài dòng đầu của dữ liệu
-print(data.head())
+    churn_predictor1 = ChurnPrediction('churn-bigml-80.csv')
+    # Tạo một đối tượng ChurnPrediction với tệp dữ liệu 'churn-bigml-80.csv'.
+    churn_predictor2 = ChurnPrediction('churn-bigml-20.csv')
+    # Tạo một đối tượng ChurnPrediction với tệp dữ liệu 'churn-bigml-20.csv'.
 
-# Kiểm tra thống kê cơ bản
-print(data.describe())
+    # Xử lý dữ liệu và hiển thị báo cáo cho tệp 1.
+    print("Báo cáo cho tệp churn-bigml-80.csv")
+    process_data(churn_predictor1)
 
-# Kiểm tra giá trị thiếu
-print(data.isnull().sum())
+    # Xử lý dữ liệu và hiển thị báo cáo cho tệp 2.
+    print("Báo cáo cho tệp churn-bigml-20.csv")
+    process_data(churn_predictor2)
 
-# Vẽ biểu đồ phân phối của biến mục tiêu (Churn)
-sns.countplot(data['Churn'])
-plt.show()
+if __name__ == "__main__":
+    main()
 
-# Bước 2: Tiền xử lý dữ liệu
-data.fillna(0, inplace=True)
-
-# Bước 3: Mã hóa biến chuỗi
-data = pd.get_dummies(data, columns=['State', 'International plan', 'Voice mail plan'], drop_first=True)
-
-# Bước 4: Tách dữ liệu thành các tập dữ liệu con
-X = data.drop('Churn', axis=1)
-y = data['Churn']
-
-# Bước 5: Chuẩn hóa dữ liệu (chỉ chuẩn hóa các đặc trưng số)
-numeric_features = X.select_dtypes(include=[np.number]).columns
-scaler = StandardScaler()
-X[numeric_features] = scaler.fit_transform(X[numeric_features])
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Bước 6: Huấn luyện mô hình
-rf_classifier = RandomForestClassifier()
-rf_classifier.fit(X_train, y_train)
-
-# Bước 7: Đánh giá mô hình
-y_pred = rf_classifier.predict(X_test)
-
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy:", accuracy)
-
-conf_matrix = confusion_matrix(y_test, y_pred)
-print("Confusion Matrix:\n", conf_matrix)
-
-class_report = classification_report(y_test, y_pred)
-print("Classification Report:\n", class_report)
